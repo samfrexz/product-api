115bc46759bb22c9ff0b33b339a9f6223167cbc0
samfrexz <samuel.oyawale@mypatricia.co>
2022-11-25 14:52:43 +0100

    update image

--- Products.js
+++ Products.js
@@ -4,9 +4,7 @@ const cartProducts = [
     fields: {
       title: "queen panel bed",
       price: 10.99,
-      image: {
-        url: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221172/products/product-1_twbqem.jpg"
-      }
+      image:  "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221172/products/product-1_twbqem.jpg"
     }
   },
   {
@@ -14,70 +12,56 @@ const cartProducts = [
     fields: {
       title: "king panel bed",
       price: 12.99,
-      image: {
-        url: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221180/products/product-2_fzgogp.jpg"
+      image: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221180/products/product-2_fzgogp.jpg"
       }
-    }
   },
   {
     id: 3,
     fields: {
       title: "single panel bed",
       price: 12.99,
-      image: {
-        url: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221188/products/product-3_ymkxiq.jpg"
+      image: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221188/products/product-3_ymkxiq.jpg"
       }
-    }
   },
   {
     id: 4,
     fields: {
       title: "twin panel bed",
       price: 22.99,
-      image: {
-        url: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221193/products/product-4_twwuct.jpg"
+      image: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221193/products/product-4_twwuct.jpg"
       }
-    }
   },
   {
     id: 5,
     fields: {
       title: "fridge",
       price: 88.99,
-      image: {
-        url: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221207/products/product-5_lpm6gi.jpg"
+      image: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221207/products/product-5_lpm6gi.jpg"
       }
-    }
   },
   {
     id: 6,
     fields: {
       title: "dresser",
       price: 32.99,
-      image: {
-        url: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221221/products/product-6_tajmao.jpg"
+      image: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221221/products/product-6_tajmao.jpg"
       }
-    }
   },
   {
     id: 7,
     fields: {
       title: "couch",
       price: 45.99,
-      image: {
-        url: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221226/products/product-7_cw7qfg.jpg"
+      image: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221226/products/product-7_cw7qfg.jpg"
       }
-    }
   },
   {
     id: 8,
     fields: {
       title: "table",
       price: 33.99,
-      image: {
-        url: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221234/products/product-8_nxkpvm.jpg"
+      image: "https://res.cloudinary.com/dslkhklbl/image/upload/v1669221234/products/product-8_nxkpvm.jpg"
       }
-    }
   },
 ]
 
